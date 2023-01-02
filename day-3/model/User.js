b1a70af12855b46db2fb064427e6b9db06cc6654
Dashka22 <dashka.nest1@gmail.com>
2023-01-02 18:46:20 +0800

    added validation

--- day-3/model/User.js
+++ day-3/model/User.js
@@ -1,12 +1,41 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  firstname: String,
+  firstname: {
+    type: String,
+    unique: [true, "Ene bol  zuwhun minii ner"],
+    required: [true, "firstName not found"],
+    minlength: [2, "1 usegtei ner gj haa bsin"],
+    maxlength: [50, "arai2 ymar urt nertei ym"],
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ["Male", "Female", "None"],
+      message: "Gender not found",
+    },
+  },
   lastname: String,
   age: Number,
   hobbies: Array,
   image: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
+
+UserSchema.path("lastname").validate((lastname) => {
+  return !/[0-9]/.test(lastname);
+}, "Neren dund too tsohij ywna hahah");
+
+UserSchema.path("firstname").validate((firstname) => {
+  return !/[0-9]/.test(firstname);
+}, "first name-d too oroh ysgui");
+
+//email validate nemeh
+
 const User = mongoose.model("User", UserSchema);
 
 export default User;
